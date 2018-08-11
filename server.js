81c4383272e6ce20ce889016ab80935a4efb8396
Brad Nolan <bnolan56@gmail.com>
2018-08-11 13:40:31 -0600

    fixing some es6 vars

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 // Dependencies
 // =============================================================
-var express = require("express");
-var bodyParser = require("body-parser");
-var path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
 
 // Sets up the Express App
 // =============================================================
-var app = express();
-var PORT = 3000;
+const app = express();
+const PORT = 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 
 // Star Wars Characters (DATA)
 // =============================================================
-var characters = [
+let characters = [
   {
     routeName: "yoda",
     name: "Yoda",
@@ -58,7 +58,7 @@ app.get("/api/characters", function(req, res) {
 
 // Displays a single character, or returns false
 app.get("/api/characters/:character", function(req, res) {
-  var chosen = req.params.character;
+  let chosen = req.params.character;
 
   console.log(chosen);
 
@@ -75,7 +75,7 @@ app.get("/api/characters/:character", function(req, res) {
 app.post("/api/characters", function(req, res) {
   // req.body hosts is equal to the JSON post sent from the user
   // This works because of our body-parser middleware
-  var newcharacter = req.body;
+  let newcharacter = req.body;
 
   // Using a RegEx Pattern to remove spaces from newCharacter
   // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
